575b5018af644f8bd7350d27e81def651b27a75a
pratapvirat2004 <pratapvirat2004@gmail.com>
2026-08-16 10:39:22 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const userId= 12332
+let accountEmail= "virat@gmail.com"
+var accountPassword ="12345"
+let accountCity="Himachal Pradesh"
+let accountState;
+
+
+console.table([userId,accountEmail,accountCity,accountPassword, accountState])
