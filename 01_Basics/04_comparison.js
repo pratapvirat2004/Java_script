20d9a57e43ad6a8715170609f40fd9eaee6de9a0
Virat Pratap <pratapvirat2004@gmail.com>
2026-08-17 21:15:31 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(2>1)
+// console.log(2>=1)
+//   console.log(null ==0)
+//   console.log(null>=0) // comparison operator is different from equality check in comparison operator the null is treated as 0 where as in  equality it is treated as null only
+//   console.log(null<0)
+
+
+  console.log("2"==2)
+  console.log("2"===2) // triple = check krta hai data type koo bhii 
