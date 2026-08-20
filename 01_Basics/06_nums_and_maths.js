cfceec54cc6bd443d26b5817513e86d2521ee1f3
Virat Pratap <pratapvirat2004@gmail.com>
2026-08-20 20:02:52 +0000

    new changes

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,12 @@
+const balance = 100
+console.log(balance)
+
+const number = new Number(100)
+console.log(number)
+
+console.log(number.toString().length)
+console.log(number.toFixed(2))
+
+const otherNumber=1323.3474
+console.log(otherNumber.toPrecision(7))
+
