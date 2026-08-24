1019d6b55b27d2464df706d2ca66263c8a8c6c0f
Virat Pratap <pratapvirat2004@gmail.com>
2026-08-24 19:18:46 +0000

    array2

---
+++ 02_basics/array2.js
@@ -0,0 +1,30 @@
+let marvel_hero=["Ironman","Hulk","Thor","Spider Man"]
+let dc_heros=["Superman","Flash","Batman"]
+
+// marvel_hero.push(dc_heros)
+// console.log(marvel_hero)
+// console.log(marvel_hero[4])
+
+// const all_heros= marvel_hero.concat(dc_heros)
+
+// console.log(all_heros)
+
+const all_heros= [...marvel_hero, ...dc_heros]
+
+// console.log(all_heros)
+
+// console.log(Array.from("Virat"))
+
+
+// console.log(Array.from({name: "Virat"}))
+
+let score1=100;
+let score2=200
+let score3=300
+
+// console.log(Array.of(score1, score2,score3))
+console.log(Array.of(score1, score2,score3))
+
+
+
+
