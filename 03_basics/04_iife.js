585d944ca720cb77ace43710f722d4939d2e0a58
Virat Pratap <pratapvirat2004@gmail.com>
2026-08-30 07:19:33 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,7 @@
+(function chai(){
+    console.log(`DB Active`)
+})(); //Ager dusera function likh rhe toh ; lagana padta hai verna error deta hai 
+
+( (name) => {
+    console.log(`DB Active two ${name}`)
+}) (`virat`)
